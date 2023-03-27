a6185a151eb7317d8334729c4fad18542d833ff1
FernandoJ152 <129100074+FernandoJ152@users.noreply.github.com>
2023-03-27 13:31:39 -0400

    Add files via upload
    "Desarrollar un programa en C que permita dado n numeros ingresados por teclado realizar las siguientes validaciones:
    a) Si el numero ingresado por teclado tiene mas de 3 dígitos mostrar por pantalla la cifra invertida: 
    Ejm: 345 → 543
    b) Mostrar por pantalla la cantidad de veces que el usuario ingreso el nro 3 y el nro 5"

---
+++ e2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int total3 = 0, total5 = 0;
+
+void invertirNum(int numero){
+	int dig, aux;
+	aux = numero;
+	do{
+	dig = aux % 10;
+	aux = aux / 10;
+	printf("%d", dig);
+	}
+	while(aux != 0);
+	printf("\n");
+	}
+	
+void sum3y5(int numero){
+	int dig, aux;
+	aux = numero;
+	do{
+	dig = aux % 10;
+	aux = aux / 10;
+	if(dig == 3){
+		total3++;
+		}
+	if(dig == 5){
+		total5++;
+			}
+		}
+	while(aux != 0);
+}
+
+int main(){
+	int n, i = 1, num, inv;
+	printf("Ingrese cantidad de numeros a ingresar: ");
+	scanf("%d", &n);
+	do{
+		printf("Numero %d: ", i);
+		scanf("%d", &num);
+		sum3y5(num);
+		if((num / 100) >= 1){
+			invertirNum(num);
+			}
+			i++;
+	}
+	while(i <= n);
+	printf("El total de 3 ingresados fue %d, el total de 5 ingresados fue %d.", total3, total5);
+	return 0;
+}
